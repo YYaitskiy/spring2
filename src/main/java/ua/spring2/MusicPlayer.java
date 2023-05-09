db5f9222e4183a920841ea0b71f48ce3f0eaac88
Yuriy <yuuurryyy@gmail.com>
2023-05-09 12:40:03 +0300

    Commit office, lesson 6 alishev

--- src/main/java/ua/spring2/MusicPlayer.java
+++ src/main/java/ua/spring2/MusicPlayer.java
@@ -3,7 +3,33 @@ package ua.spring2;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MusicPlayer() {
+    }
+
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
